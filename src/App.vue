612afd23a7bdfeedbc3c676d8f4957224f80b7e9
xindong.xie <779788582@qq.com>
2020-09-10 00:17:34 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+   {{greeting}} world!
+    <Header/>
+
+  
+  </div>   
+</template>
+
+<script>
+import Header from './components/header/header.vue'
+
+  export default{
+    data() {
+      return {
+        greeting:"xxd"
+      }
+    },
+    components:{
+      Header
+    }
+  }
+</script>
+
+
+<style scoped>
+  p{
+    font-size: 2em;
+    text-align: center;
+  }
+</style>
